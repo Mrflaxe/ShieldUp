03ff36f9602cd6b870a5a1023fbb3c5e8b0f5cab
Mrflaxe <dmitriyclon@gmail.com>
2020-09-16 08:36:36 +0300

    forgotten to register the PlayerQuitListener

--- src/main/java/ru/mrflaxe/shieldup/ShieldUp.java
+++ src/main/java/ru/mrflaxe/shieldup/ShieldUp.java
@@ -4,9 +4,10 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import ru.mrflaxe.shieldup.listeners.InventoryClickListener;
 import ru.mrflaxe.shieldup.listeners.InventoryOpenListener;
-import ru.mrflaxe.shieldup.listeners.PlayerSwapHandItemsListener;
 import ru.mrflaxe.shieldup.listeners.PlayerDeathListener;
 import ru.mrflaxe.shieldup.listeners.PlayerInteractListener;
+import ru.mrflaxe.shieldup.listeners.PlayerQuitListener;
+import ru.mrflaxe.shieldup.listeners.PlayerSwapHandItemsListener;
 import ru.mrflaxe.shieldup.provider.ShieldProvider;
 
 public class ShieldUp extends JavaPlugin {
@@ -27,6 +28,7 @@ public class ShieldUp extends JavaPlugin {
 		new PlayerDeathListener(shieldProvider).register(this);
 		new PlayerInteractListener(shieldProvider).register(this);
 		new InventoryClickListener(shieldProvider).register(this);
+		new PlayerQuitListener(shieldProvider).register(this);
 	}
 	
 }
